461f46774624901d5a60288dae734c82e1f794a6
Tilo Heidasch <tilo.heidasch@capgemini.com>
2020-06-23 10:54:41 +0200

    Adds Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80);
+  const options = new DocumentBuilder()
+    .setTitle('Emoji Search API')
+    .setDescription('An API that lets you search the available Unicode Emojis in different languages')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  const port = 80;
+  await app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port + '/');
+  });
 }
 bootstrap();
