4828bd230cee5ba754e6cd49c5ca38e78bbb1630
Tilo Heidasch <tilo.heidasch@capgemini.com>
2020-06-23 19:53:28 +0200

    Adds Request Logging

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Logger } from '@nestjs/common';
 import { EmojiService } from './emoji.service';
 import { Emoji } from './emoji';
 import { ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { Language } from './lang';
+import { v4 as uuid } from 'uuid';
+
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly emojiService: EmojiService) { }
 
   @Get()
@@ -15,6 +18,11 @@ export class AppController {
   @ApiQuery({ name: 'minRelevance', description: 'The minimum required relevance - 1 is most relevant, 0 is least relevant', required: false, example: 0.3 })
   @ApiResponse({ description: 'The emojis, determined by the given criteria', type: [Emoji] })
   async getEmojis(@Query('lang') lang?: Language, @Query('search') search?: string, @Query('max') max?: number, @Query('minRelevance') minRelevance?: number): Promise<Emoji[]> {
-    return this.emojiService.getEmojis(lang, search, max, minRelevance);
+    const start = new Date();
+    const id = uuid();
+    const emojis: Emoji[] = await this.emojiService.getEmojis(lang, search, max, minRelevance);
+    const end = new Date();
+    this.logger.log({ request: { id, time: { start, end, delta: end.valueOf() - start.valueOf() + 'ms' }, params: { lang, search, max, minRelevance }, result: emojis.length } }, AppController.name + '|' + this.getEmojis.name);
+    return emojis;
   }
 }
